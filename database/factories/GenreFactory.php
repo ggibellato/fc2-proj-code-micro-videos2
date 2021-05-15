cf71d696c3553d53eaa4ae8f887a1f8ffb27d137
Gleison Gibellato da Silva <ggibellato@gmail.com>
2021-05-15 12:43:16 +0100

    Criando recurso Category

---
+++ database/factories/GenreFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Models\Genre;
+use Faker\Generator as Faker;
+
+$factory->define(Genre::class, function (Faker $faker) {
+    return [
+        'name' => $faker->randomElement(['Ação','Aventura','Cinema de arte','Chanchada','Comédia','Comédia de ação','Comédia de terror','Comédia dramática','Comédia romântica','Dança','Documentário','Drama','Espionagem','Faroeste','Fantasia']),
+    ];
+});
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::group(['namespace' => 'Api'], function(){
+    Route::resource('categories', 'CategoryController', ['except' => ['create', 'edit']]);
+    Route::resource('generes', 'GenreController', ['except' => ['create', 'edit']]);
+});
+
+
