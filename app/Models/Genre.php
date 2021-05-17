e77bf2f45cc6d771b333f4e85b7690c0f587fbc9
Gleison Gibellato da Silva <ggibellato@gmail.com>
2021-05-17 09:33:20 +0100

    changed to use uuid

--- app/Models/Genre.php
+++ app/Models/Genre.php
@@ -3,8 +3,16 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Genre extends Model
 {
+    use SoftDeletes, Traits\Uuid;
     protected $fillable = [ 'name', 'is_active'];
+    protected $dates = ['deleted_ad'];
+    protected $casts = [
+        'id' => 'string'
+    ];
+    public $incrementing = false;
+    public $keyType = 'string';
 }
